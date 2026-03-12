0698bc33188c7fbadf493eb41fa2dbdcf56fc3ce
usuario-27554 <will.dutra26@gmail.com>
2026-03-12 16:12:43 -0300

    fix(tatico): ajuste na ordem dos jogadores em campo

--- src/app/campo/componentes/jogador/jogar.ts
+++ src/app/campo/componentes/jogador/jogar.ts
@@ -3,11 +3,14 @@ export class Jogador {
   camisa: number;
   nome: string;
   posicao: Posicao;
-  constructor(id: number, camisa: number, nome: string, posicao: Posicao) {
+  ordem_campo: OrdemCampo;
+
+  constructor(id: number, camisa: number, nome: string, posicao: Posicao, ordem_campo: OrdemCampo) {
     this.id = id;
     this.camisa = camisa;
     this.nome = nome;
     this.posicao = posicao;
+    this.ordem_campo = ordem_campo;
   }
 }
 
@@ -19,4 +22,17 @@ export enum Posicao {
   meia,
   atacante,
   zagueiro,
+}
+
+export enum OrdemCampo {
+  goleiro = 1,
+  lateral_direito = 5,
+  lateral_esquerdo = 2,
+  zagueiro_destro = 3,
+  zagueiro_canhoto = 4,
+  meia_canhoto = 6,
+  volante = 7,
+  meia_destro = 8,
+  atacante_canhoto = 9,
+  atacante_destro = 10,
 }
--- src/app/campo/tatico/tatico.component.ts
+++ src/app/campo/tatico/tatico.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { JogadorComponent } from '../componentes/jogador/jogador.component';
-import { Jogador, Posicao } from '../componentes/jogador/jogar';
+import { Jogador, OrdemCampo, Posicao } from '../componentes/jogador/jogar';
 
 @Component({
   selector: 'app-tatico',
@@ -10,17 +10,83 @@ import { Jogador, Posicao } from '../componentes/jogador/jogar';
 })
 export class TaticoComponent {
   jogadores: Jogador[] = [
-    { id: 1, nome: 'Hugo Souza', camisa: 1, posicao: Posicao.goleiro },
-    { id: 2, nome: 'Matheuzinho', camisa: 2, posicao: Posicao.lateral_direito },
-    { id: 23, nome: 'Matheus Bidu', camisa: 23, posicao: Posicao.lateral_esquerdo },
-    { id: 13, nome: 'Gustavo Henrique', camisa: 13, posicao: Posicao.zagueiro },
-    { id: 13, nome: 'Gabriel Paulista', camisa: 13, posicao: Posicao.zagueiro },
-    { id: 45, nome: 'Allan', camisa: 45, posicao: Posicao.volante },
-    { id: 48, nome: 'André Luiz', camisa: 28, posicao: Posicao.meia },
-    { id: 7, nome: 'Breno Bidon', camisa: 7, posicao: Posicao.meia },
-    { id: 8, nome: 'Rodrigo Garro', camisa: 8, posicao: Posicao.meia },
-    { id: 10, nome: 'Memphis', camisa: 10, posicao: Posicao.atacante },
-    { id: 9, nome: 'Yuri Alberto', camisa: 9, posicao: Posicao.atacante },
+    {
+      id: 1,
+      nome: 'Hugo Souza',
+      camisa: 1,
+      posicao: Posicao.goleiro,
+      ordem_campo: OrdemCampo.goleiro,
+    },
+    {
+      id: 2,
+      nome: 'Matheuzinho',
+      camisa: 2,
+      posicao: Posicao.lateral_direito,
+      ordem_campo: OrdemCampo.lateral_direito,
+    },
+    {
+      id: 23,
+      nome: 'Matheus Bidu',
+      camisa: 23,
+      posicao: Posicao.lateral_esquerdo,
+      ordem_campo: OrdemCampo.lateral_esquerdo,
+    },
+    {
+      id: 13,
+      nome: 'Gustavo Henrique',
+      camisa: 13,
+      posicao: Posicao.zagueiro,
+      ordem_campo: OrdemCampo.zagueiro_canhoto,
+    },
+    {
+      id: 13,
+      nome: 'Gabriel Paulista',
+      camisa: 13,
+      posicao: Posicao.zagueiro,
+      ordem_campo: OrdemCampo.zagueiro_destro,
+    },
+    {
+      id: 45,
+      nome: 'Allan',
+      camisa: 45,
+      posicao: Posicao.volante,
+      ordem_campo: OrdemCampo.volante,
+    },
+    {
+      id: 48,
+      nome: 'André Luiz',
+      camisa: 28,
+      posicao: Posicao.meia,
+      ordem_campo: OrdemCampo.meia_destro,
+    },
+    {
+      id: 7,
+      nome: 'Breno Bidon',
+      camisa: 7,
+      posicao: Posicao.meia,
+      ordem_campo: OrdemCampo.meia_canhoto,
+    },
+    {
+      id: 8,
+      nome: 'Rodrigo Garro',
+      camisa: 8,
+      posicao: Posicao.meia,
+      ordem_campo: OrdemCampo.meia_canhoto,
+    },
+    {
+      id: 10,
+      nome: 'Memphis',
+      camisa: 10,
+      posicao: Posicao.atacante,
+      ordem_campo: OrdemCampo.atacante_destro,
+    },
+    {
+      id: 9,
+      nome: 'Yuri Alberto',
+      camisa: 9,
+      posicao: Posicao.atacante,
+      ordem_campo: OrdemCampo.atacante_canhoto,
+    },
   ];
 
   get goleiro() {
@@ -28,19 +94,25 @@ export class TaticoComponent {
   }
 
   get defesa() {
-    return this.jogadores.filter(
-      (g) =>
-        g.posicao == Posicao.lateral_direito ||
-        g.posicao == Posicao.lateral_esquerdo ||
-        g.posicao == Posicao.zagueiro,
-    );
+    return this.jogadores
+      .filter(
+        (g) =>
+          g.posicao == Posicao.lateral_direito ||
+          g.posicao == Posicao.lateral_esquerdo ||
+          g.posicao == Posicao.zagueiro,
+      )
+      .sort((a, b) => a.ordem_campo - b.ordem_campo);
   }
 
   get meio() {
-    return this.jogadores.filter((g) => g.posicao == Posicao.meia || g.posicao == Posicao.volante);
+    return this.jogadores
+      .filter((g) => g.posicao == Posicao.meia || g.posicao == Posicao.volante)
+      .sort((a, b) => a.ordem_campo - b.ordem_campo);
   }
 
   get ataque() {
-    return this.jogadores.filter((g) => g.posicao == Posicao.atacante);
+    return this.jogadores
+      .filter((g) => g.posicao == Posicao.atacante)
+      .sort((a, b) => a.ordem_campo - b.ordem_campo);
   }
 }
