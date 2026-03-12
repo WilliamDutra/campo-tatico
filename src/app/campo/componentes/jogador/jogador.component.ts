331209a3d12dd8b0a849e7f965e0d28172878951
usuario-27554 <will.dutra26@gmail.com>
2026-03-12 16:22:00 -0300

    feat(tatico): inclusão do drag and drop no componente jogador

--- src/app/campo/componentes/jogador/jogador.component.ts
+++ src/app/campo/componentes/jogador/jogador.component.ts
@@ -1,9 +1,10 @@
 import { Component, input, output, signal } from '@angular/core';
 import { Jogador } from './jogar';
+import { CdkDrag } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-jogador',
-  imports: [],
+  imports: [CdkDrag],
   templateUrl: './jogador.component.html',
   styleUrl: './jogador.component.css',
 })
